ac0ca0176b77271bca26354bd3b4e0592c9cb313
Gibbsface <cole.gibbs@gmail.com>
2022-11-23 23:36:51 -0600

    polish to merge

--- scripts/press.js
+++ scripts/press.js
@@ -1,11 +1,10 @@
 const d = new Date();
 
 //based on the date, we need the following constants:
-    //day out of 366 (leap year)
     //day out of 31
     //day out of 7 (not always the day of the week)
+    //day out of 365 (leap year)
     //parity of week (even/odd out of 52)
-
 
 //get day for 31 cycle
     //easy calculation
@@ -15,21 +14,33 @@ const cycle_31 = d.getDate();
     //I know this looks weird, but it cycles 1-7 instead of 0-6
 const cycle_7 = 1 + ((cycle_31-1) % 7);
 
-//calculate day for 366 cycle
-// TODO
+//calculate day for 365 cycle
+    //First, get raw day of year
+const year_start = new Date(d.getFullYear(), 0, 0, 0 ,0);
+var raw_day_of_year = Math.floor((d.getTime()-year_start.getTime()) / 1000 / 60 / 60 / 24);
 
-//Need to check if leap year
-//if leap year, calc as normal (366)
-//OR if not leap year and before March 1, also calc as normal
-//else if not leap year and after Feb 28, then add 1 to normal dates
+    //Next, check isLeap
+const year = d.getFullYear();
+const isLeap = ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0);
 
+    //adjust for leap year and leap day
+if(isLeap && raw_day_of_year > 60){
+    raw_day_of_year--;
+} else if(isLeap && raw_day_of_year == 60){
+    raw_day_of_year = 0;
+}
 
+    //Finally, assign cycle 365
+const cycle_365 = raw_day_of_year;
 
+//calculate week parity
+const week_parity = cycle_31 % 2;
 
 //print all
 console.log("Today is: " + d);
 console.log("Day of the month: " + cycle_31);
 console.log("Day of the 'week': " + cycle_7);
-//console.log("Day of the year: " + cycle_366);
-//console.log("Week parity: " + week_parity);
+console.log("Day of the year: " + cycle_365);
+console.log("Week parity: " + week_parity);
 console.log("End of test\n");
+
